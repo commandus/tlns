22180a9e29e5bd64b8c6de645d88aaa6d9f0cae7
andrei <andrei.i.ivanov@gmail.com>
2024-01-06 14:59:36 +0900

    Win

--- cli-main-check.cpp
+++ cli-main-check.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 
 #include <sstream>
-#include <cstring>
 
 #ifdef _MSC_VER
 #include <WinSock2.h>
@@ -11,9 +10,8 @@
 #include "argtable3/argtable3.h"
 
 #include "lorawan/lorawan-error.h"
-#include "lorawan/lorawan-msg.h"
 #include "lorawan/lorawan-string.h"
-
+#include "lorawan/task/message-queue.h"
 
 const char *programName = "tlns-check";
 #define DEF_PORT 4244
@@ -30,7 +28,7 @@ public:
 
     }
 
-    std::string toString() {
+    std::string toString() const {
         std::stringstream ss;
         ss << "Verbose: " << verbose << "\n";
         return ss.str();
@@ -41,6 +39,8 @@ static CheckParams params;
 
 static void run()
 {
+    MessageQueue q;
+    q.setSize(1, 10);
 }
 
 int main(int argc, char **argv) {
@@ -69,7 +69,7 @@ int main(int argc, char **argv) {
 			arg_print_errors(stderr, a_end, programName);
 		std::cerr << "Usage: " << programName << std::endl;
 		arg_print_syntax(stderr, argtable, "\n");
-		std::cerr << "tlns chack utility" << std::endl;
+		std::cerr << "tlns check utility" << std::endl;
 		arg_print_glossary(stderr, argtable, "  %-27s %s\n");
 		arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
 		return ERR_CODE_COMMAND_LINE;

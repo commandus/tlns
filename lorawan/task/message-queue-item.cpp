dac3a37f970a6a6f239da9445caa655167720df7
Andrei <andrei.i.ivanov@commandus.com>
2024-09-19 16:07:14 +0900

    Fix formatting

--- lorawan/task/message-queue-item.cpp
+++ lorawan/task/message-queue-item.cpp
@@ -58,13 +58,15 @@ std::string MessageQueueItem::toString() const
 {
     std::stringstream ss;
     std::time_t t = std::chrono::system_clock::to_time_t(firstGatewayReceived);
-    ss << "{\"time\": " << time2string(t) << "\", \"radio\": " << radioPacket.toString()
-        << ", \"metadata\": [";
+    ss << "{\"received\": \"" << time2string(t) << "\", \"radio\": " << radioPacket.toString();
+    if (radioPacket.payloadSize)
+        ss << ", \"payload\": \"" << hexString((const char *) radioPacket.data.downlink.payload(), radioPacket.payloadSize) << "\"";
+    ss << ", \"gateways\": [";
     for (auto it : metadata) {
         ss << " " <<  gatewayId2str(it.first)
             << ": " << BANDWIDTH2String(it.second.rx.bandwidth);
     }
-    ss << "]";
+    ss << "]}";
     return ss.str();
 }
 
